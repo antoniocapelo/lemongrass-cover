5263cf16217c41ba0c0c0a54ee17a93da3914c5b
Capelo <antonio.c.capelo@gmail.com>
2019-05-27 19:16:22 +0100

    feat: moving

--- src/gl.js
+++ src/gl.js
@@ -1,4 +1,3 @@
-//@flow
 import React, { Component } from "react";
 import { Shaders, Node, GLSL } from "gl-react";
 import { Surface } from "gl-react-dom";
@@ -12,63 +11,64 @@ const getColor = color => {
 
 const shaders = Shaders.create({
   base: {
-    // uniforms are variables from JS. We pipe blue uniform into blue output color
     frag: GLSL`
-precision highp float;
-varying vec2 uv;
-uniform vec3 color;
-uniform float aspect;
-uniform float amplitude;
-uniform float time;
-  uniform sampler2D t;
+      precision highp float;
+      varying vec2 uv;
+      uniform vec3 color;
+      uniform float aspect;
+      uniform float amplitude;
+      uniform sampler2D t;
 
-void main() {
- gl_FragColor = vec4(color, 1.0);
- float margin = 0.10;
- vec2 bl = step(vec2(margin),uv);       // bottom-left
-vec2 tr = step(vec2(margin),1.0-uv);   // top-right
-float isInside = 1. - (bl.x * bl.y * tr.x * tr.y);
+      void main() {
+      gl_FragColor = vec4(color, 1.0);
+      float margin = 0.10;
+      vec2 bl = step(vec2(margin),uv);       // bottom-left
+      vec2 tr = step(vec2(margin),1.0-uv);   // top-right
+      float isInside = 1. - (bl.x * bl.y * tr.x * tr.y);
 
-vec2 center = uv - 0.5;
-center.x *= aspect;
+      vec2 center = uv - 0.5;
+      center.x *= aspect;
 
-float dist = length(center);
+      float dist = length(center);
 
-float insideCircle = smoothstep(0.315, 0.3125, dist);
+      float insideCircle = smoothstep(0.315, 0.3125, dist);
 
-float drawBg = insideCircle + isInside;
-vec2 distortedUv = uv;
-distortedUv.y += cos(amplitude * (uv.x - 0.5) * 100.) / 30.;
+      float drawBg = insideCircle + isInside;
+      vec2 distortedUv = uv;
+      distortedUv.y += cos(amplitude * (uv.x - 0.5) * 100.) / 30.;
 
-  gl_FragColor = mix(
-    texture2D(t, distortedUv),
-    vec4(color, 1.0),
-    drawBg
-  );
-
-}`
+      gl_FragColor = mix(
+        texture2D(t, distortedUv),
+        vec4(color, 1.0),
+        drawBg
+      ); 
+    }`
   },
   lines: {
     frag: GLSL`
-precision highp float;
-uniform vec3 bgColor;
-varying vec2 uv;
-void main() {
-  vec3 color = mix(vec3(1., 1., 1.), bgColor, 1. - uv.y);
-  float f = fract(uv.y * 40.) * 0.57;  
-  float pct = step(f, 0.5);
-  pct = step(f, 0.5);
-  gl_FragColor = vec4(mix(color, bgColor, pct), 1.0);
-}`
+      precision highp float;
+      uniform vec3 bgColor;
+      varying vec2 uv;
+      uniform float len;
+
+      void main() {
+        vec3 color = mix(vec3(1., 1., 1.), bgColor, 1. - uv.y);
+        float f = fract(uv.y * 40.) * 0.57;  
+        float pct = step(f, 0.5);
+        pct = step(step(uv.x, len), pct);
+        pct = step(step(1.-len, uv.x), pct);
+        gl_FragColor = vec4(mix(color, bgColor, pct), 1.0);
+        // gl_FragColor = vec4(bgColor, 1.0);
+      }`
   }
 });
 
 // We can make a <HelloBlue blue={0.5} /> that will render the concrete <Node/>
 export class Base extends Component {
   render() {
-    const { color, amplitude, children: t, aspect, time } = this.props;
+    const { color, amplitude, children: t, aspect } = this.props;
     return (
-      <Node shader={shaders.base} uniforms={{ t, color, amplitude, aspect, time }} />
+      <Node shader={shaders.base} uniforms={{ t, color, amplitude, aspect }} />
     );
   }
 }
@@ -76,19 +76,20 @@ export class Base extends Component {
 // We can make a <HelloBlue blue={0.5} /> that will render the concrete <Node/>
 export class Lines extends Component {
   render() {
-    const { bgColor } = this.props;
-    return <Node shader={shaders.lines} uniforms={{ bgColor }} />;
+    const { bgColor, len } = this.props;
+    return <Node shader={shaders.lines} uniforms={{ bgColor, len }} />;
   }
 }
 
-const initialAmp = 0.2;
+const initialAmp = 0.0;
 
 // Our example will pass the slider value to HelloBlue
 class GL extends Component {
   state = {
     width: 0,
     height: 0,
-    amplitude: initialAmp
+    amplitude: initialAmp,
+    len: 1,
   };
   color = "#9cbfa1";
   componentDidMount() {
@@ -119,10 +120,10 @@ class GL extends Component {
     });
   };
   render() {
-    const { amplitude } = this.state;
+    const { amplitude, len } = this.state;
     // const { time } = this.props;
-    const time = performance.now() / 2000
-
+    const time = performance.now() / 2000;
+    console.log(len);
 
     return (
       <>
@@ -130,10 +131,10 @@ class GL extends Component {
           <Base
             color={getColor(this.color)}
             aspect={this.state.width / this.state.height}
-            amplitude={amplitude}
+            amplitude={amplitude} 
             time={time}
           >
-            <Lines bgColor={getColor(this.color)} />
+            <Lines bgColor={getColor(this.color)} len={len}/>
           </Base>
         </Surface>
         <input
@@ -142,8 +143,8 @@ class GL extends Component {
           min={0}
           max={1}
           step={0.01}
-          value={this.state.amplitude}
-          onChange={ev => this.setState({ amplitude: ev.target.value })}
+          value={this.state.len}
+          onChange={ev => this.setState({ len: ev.target.value })}
         />
       </>
     );
@@ -151,4 +152,4 @@ class GL extends Component {
   static defaultProps = { blue: 0.5 };
 }
 
-export default timeLoopHOC(GL);
+export default (GL);
